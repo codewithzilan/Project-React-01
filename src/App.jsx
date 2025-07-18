59df438bb5877ab13de8c8a02fffd07154c0e798
codewithzilan <mohammadzilan31@gamil.com>
2025-07-19 02:06:50 +0600

    Cloning

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Service from './layouts/Service'
 import Choose from './layouts/Choose'
 import CardSection from './layouts/CardSection'
 import Impact from './layouts/Impact'
-import Text from './layouts/Text'
+
 
 
 
@@ -21,7 +21,6 @@ const App = () => {
     <Choose/>
     <CardSection/>
     <Impact/>
-    <Text/>
 
 
     </>
